2c352250f6b3c41b5e2e16459ce285ef6451acf2
Arzh42 <hugo@chataigner.com>
2020-01-27 21:58:35 +0100

    Add load file through parse

--- src/obj_model.js
+++ src/obj_model.js
@@ -14,45 +14,49 @@ class ObjModel extends Model{
   constructor(props){
     super(props)
   }
+  loaded(obj3d) {
+      var bound_box = this.computeBoundingBox(obj3d);
+      // debugger
+      var front = bound_box.max;
+
+      //debugger
+
+      var cz = bound_box.max.z - bound_box.min.z;
+
+      this.camera.position.set(0, 0, front.z+cz*1.5);
+
+
+      this.initControl();
+
+
+
+      //this.scene.add(obj3d);
+      this.obj3d = obj3d;
+
+      this.props.onLoad && this.props.onLoad()
+
+  }
   load3dModel(){
-    var {src,texPath} = this.props;
- 
-    if(!src) return false
+    var {src,texPath, json_data} = this.props;
+
     // instantiate a loader
     // load a resource
     var obj_loader = new THREE.OBJLoader()
+      // decide how to load the data
+      if (json_data) {
+          obj_loader.parse(json_data,this.loaded);
+      }
     obj_loader.load(
       // resource URL
       src,
       // called when resource is loaded
-      obj3d => {
-
-        var bound_box = this.computeBoundingBox(obj3d);
-        // debugger
-        var front = bound_box.max;
-
-        //debugger
-
-        var cz = bound_box.max.z - bound_box.min.z;
-
-        this.camera.position.set(0, 0, front.z+cz*1.5);
-
-
-        this.initControl();
-        
-
-        
-        //this.scene.add(obj3d);
-        this.obj3d = obj3d;
-
-        this.props.onLoad && this.props.onLoad()
-
-      },
+        this.loaded
+      ,
       // called when loading is in progresses
       xhr =>{
         this.props.onProgress && this.props.onProgress(xhr)
         // console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-      } 
+      }
     );
 
   }
